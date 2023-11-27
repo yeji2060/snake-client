3507c7c17ffd1b5f1e98d34fdbda3e63164c9f32
Yeji Ha <yeji206000@gmail.com>
2023-11-27 15:09:41 -0800

    refractor input module

--- client.js
+++ client.js
@@ -1,7 +1,30 @@
-const connect = require('./play');
 
-connect.setEncoding("utf8");
-connect.on("data", (data) => {
-  console.log(data);
-})
+// establishes a connection with the game server
+const connect = function () {
+
+  const net = require("net");
+
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: YJH");
+
+    //conn.write("Move: up");    
+  })
+
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+  return conn;
+};
+
+module.exports = { connect };
 
--- play.js
+++ play.js
@@ -1,50 +1,8 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-    conn.write("Name: YJH");
-
-    //conn.write("Move: up");    
-  })
-
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  return conn;
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput)
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-    if(key === '\u0003') {
-      process.exit();
-    }
-  }
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 
 console.log("Connecting ...");
 connect();
+
 setupInput();
-
-
-module.exports = connect;
-
-

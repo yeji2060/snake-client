2a0f6e9b7d882f42186eadb1f8a68a89dccabde5
Yeji Ha <yeji206000@gmail.com>
2023-11-27 02:50:34 -0800

    separated handle function

--- play.js
+++ play.js
@@ -29,15 +29,15 @@ const setupInput = function () {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput => {
-    stdin.on('data', (key) => {
-      if(key === '\u0003') {
-        process.exit();
-      }
-    })
-  })
+  stdin.on("data", handleUserInput)
   return stdin;
 };
+
+const handleUserInput = function (key) {
+    if(key === '\u0003') {
+      process.exit();
+    }
+  }
 
 
 console.log("Connecting ...");
